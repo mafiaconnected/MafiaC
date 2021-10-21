bfaed483915f45bfb8e91af1c8eaaff13fefef07
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-10-21 00:02:17 -0500

    Add more smoothed values to veh netblender

--- Projects/Hacks/MultiplayerMod/Elements/NetBlenderVehicle.cpp
+++ Projects/Hacks/MultiplayerMod/Elements/NetBlenderVehicle.cpp
@@ -55,3 +55,51 @@ void CNetBlenderVehicle::SetTurnSpeed(const CVector3D& vecTurnSpeed)
 {
 	m_pEntity->SetRotationVelocity(vecTurnSpeed);
 }
+
+float CNetBlenderVehicle::GetEngineRPM()
+{
+	return m_pEntity->GetEngineRPM();
+}
+
+void CNetBlenderVehicle::SetEngineRPM(float engineRPM)
+{
+	m_pEntity->SetEngineRPM(engineRPM);
+}
+
+float CNetBlenderVehicle::GetWheelAngle()
+{
+	return m_pEntity->GetWheelAngle();
+}
+
+void CNetBlenderVehicle::SetWheelAngle(float wheelAngle)
+{
+	m_pEntity->SetWheelAngle(wheelAngle);
+}
+
+void CNetBlenderVehicle::UpdateTargetWheelAngle()
+{
+	if (m_WheelAngle.HasTarget())
+	{
+		float vecCurrentWheelAngle;
+		vecCurrentWheelAngle = GetWheelAngle();
+
+		float vecNewWheelAngle = vecCurrentWheelAngle;
+		//m_WheelAngle.Update(vecNewWheelAngle, m_fWheelAngleMaxError);
+
+		SetWheelAngle(vecNewWheelAngle);
+	}
+}
+
+void CNetBlenderVehicle::UpdateTargetEngineRPM()
+{
+	if (m_EngineRPM.HasTarget())
+	{
+		float vecCurrentEngineRPM;
+		vecCurrentEngineRPM = GetEngineRPM();
+
+		float vecNewEngineRPM = vecCurrentEngineRPM;
+		///m_EngineRPM.Update(vecNewEngineRPM, m_fEngineRPMMaxError);
+
+		SetWheelAngle(vecNewEngineRPM);
+	}
+}
--- Projects/Hacks/MultiplayerMod/Elements/NetBlenderVehicle.h
+++ Projects/Hacks/MultiplayerMod/Elements/NetBlenderVehicle.h
@@ -9,6 +9,16 @@ public:
 
 	CClientVehicle* m_pEntity;
 
+	CInterpolation m_EngineRPM;
+	CInterpolation m_WheelAngle;
+
+	float m_fWheelAngleMaxError;
+	float m_fEngineRPMMaxError;
+	float m_fMaxInterpolationAmount;
+
+	void UpdateTargetEngineRPM();
+	void UpdateTargetWheelAngle();
+
 	virtual void GetPosition(CVector3D& vecPos) override;
 	virtual void SetPosition(const CVector3D& vecPos) override;
 
@@ -20,4 +30,16 @@ public:
 
 	virtual void GetTurnSpeed(CVector3D& vecTurnSpeed) override;
 	virtual void SetTurnSpeed(const CVector3D& vecTurnSpeed) override;
+
+	virtual float GetTargetEngineRPM();
+	virtual void SetTargetEngineRPM(float engineRPM);
+
+	virtual float GetTargetWheelAngle();
+	virtual void SetTargetWheelAngle(float wheelAngle);
+
+	virtual float GetEngineRPM();
+	virtual void SetEngineRPM(float engineRPM);
+
+	virtual float GetWheelAngle();
+	virtual void SetWheelAngle(float wheelAngle);
 };
