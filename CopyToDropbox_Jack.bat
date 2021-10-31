5201c18c8484a17cd1a97b0c6fca2f6cfd7d74a6
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-10-31 14:47:20 -0500

    Add dropbox copy utils

---
+++ CopyToDropbox_Jack.bat
@@ -0,0 +1,24 @@
+copy GTAC.tar "%dropbox%\MafiaC\Client\"
+copy GTAC.tar "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\"
+copy GTAC.tar "%dropbox%\MafiaC (Testers)\Builds\Jack\Client\"
+
+cd Lib\x86\v142_static
+
+del "%dropbox%\MafiaC\Client\*.exe"
+del "%dropbox%\MafiaC\Client\*.dll"
+
+REM Staff Folder
+del "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\*.exe"
+del "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\*.dll"
+copy Debug\*.exe "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\"
+copy Debug\*.dll "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\"
+copy RelWithDebInfo\*.exe "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\"
+copy RelWithDebInfo\*.dll "%dropbox%\MafiaC (Staff)\Builds\Jack\Client\"
+
+REM Tester Folder
+del "%dropbox%\MafiaC (Testers)\Builds\Jack\Client\*.exe"
+del "%dropbox%\MafiaC (Testers)\Builds\Jack\Client\*.dll"
+copy RelWithDebInfo\*.exe "%dropbox%\MafiaC (Testers)\Builds\Jack\Client\"
+copy RelWithDebInfo\*.dll "%dropbox%\MafiaC (Testers)\Builds\Jack\Client\"
+
+pause
---
+++ CopyToDropbox_Vortrex.bat
@@ -0,0 +1,19 @@
+copy GTAC.tar "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\"
+copy GTAC.tar "%dropbox%\MafiaC (Testers)\Builds\Vortrex\Client\"
+
+cd Lib\x86\v141_xp_static
+
+REM Staff Folder
+del "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\*.exe"
+del "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\*.dll"
+copy Debug\*.exe "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\"
+copy Debug\*.dll "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\"
+copy RelWithDebInfo\*.exe "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\"
+copy RelWithDebInfo\*.dll "%dropbox%\MafiaC (Staff)\Builds\Vortrex\Client\"
+
+REM Tester Folder
+del "%dropbox%\MafiaC (Testers)\Builds\Vortrex\Client\*.exe"
+del "%dropbox%\MafiaC (Testers)\Builds\Vortrex\Client\*.dll"
+copy RelWithDebInfo\*.exe "%dropbox%\MafiaC (Testers)\Builds\Vortrex\Client\"
+copy RelWithDebInfo\*.dll "%dropbox%\MafiaC (Testers)\Builds\Vortrex\Client\"
+pause
