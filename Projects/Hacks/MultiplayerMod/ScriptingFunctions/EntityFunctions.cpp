8ec09eb55a14fe02014fe044ae46812e48c6cbb5
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2021-12-14 12:05:01 -0600

    Entity heading and model properties

--- Projects/Hacks/MultiplayerMod/ScriptingFunctions/EntityFunctions.cpp
+++ Projects/Hacks/MultiplayerMod/ScriptingFunctions/EntityFunctions.cpp
@@ -88,6 +88,39 @@ static bool FunctionEntitySetRotation(IScriptState* pState, int argc, void* pUse
 	return false;
 }
 
+static bool FunctionEntityGetHeading(IScriptState* pState, int argc, void* pUser)
+{
+	CMafiaClientManager* pClientManager = (CMafiaClientManager*)pUser;
+
+	CClientEntity* pClientEntity;
+
+	if (!pState->GetThis(pClientManager->m_pClientEntityClass, &pClientEntity))
+		return false;
+
+	pState->ReturnNumber(pClientEntity->GetHeading());
+	return false;
+}
+
+static bool FunctionEntitySetHeading(IScriptState* pState, int argc, void* pUser)
+{
+	CMafiaClientManager* pClientManager = (CMafiaClientManager*)pUser;
+
+	CClientEntity* pClientEntity;
+
+	if (!pState->GetThis(pClientManager->m_pClientEntityClass, &pClientEntity))
+		return false;
+
+	float heading;
+	if (!pState->CheckNumber(0, heading))
+		return false;
+
+	if (pClientEntity->SetHeading(heading))
+		return true;
+
+	pState->Error(_gstr("entity not spawned"));
+	return false;
+}
+
 static bool FunctionEntityGetModel(IScriptState* pState, int argc, void* pUser)
 {
 	CMafiaClientManager* pClientManager = (CMafiaClientManager*)pUser;
@@ -104,12 +137,32 @@ static bool FunctionEntityGetModel(IScriptState* pState, int argc, void* pUser)
 	return true;
 }
 
+static bool FunctionEntitySetModel(IScriptState* pState, int argc, void* pUser)
+{
+	CMafiaClientManager* pClientManager = (CMafiaClientManager*)pUser;
+
+	CClientEntity* pClientEntity;
+
+	if (!pState->GetThis(pClientManager->m_pClientEntityClass, &pClientEntity))
+		return false;
+
+	const GChar* mdl = pState->CheckString(0);
+	if (!mdl) return false;
+
+	pClientEntity->SetModel(mdl);
+
+	pState->ReturnString(mdl);
+
+	return true;
+}
+
 void CScriptingFunctions::RegisterEntityFunctions(Galactic3D::CScripting* pScripting, CClientGame* pClientGame)
 {
 	auto pClientManager = pClientGame->m_pClientManager;
 
 	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("position"), ARGUMENT_VECTOR3D, FunctionEntityGetPosition, FunctionEntitySetPosition);
 	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("rotation"), ARGUMENT_VECTOR3D, FunctionEntityGetRotation, FunctionEntitySetRotation);
-	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("model"), ARGUMENT_STRING, FunctionEntityGetModel);
-	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("modelIndex"), ARGUMENT_STRING, FunctionEntityGetModel);
+	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("heading"), ARGUMENT_VECTOR3D, FunctionEntityGetHeading, FunctionEntitySetHeading);
+	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("model"), ARGUMENT_STRING, FunctionEntityGetModel, FunctionEntitySetModel);
+	pClientManager->m_pClientEntityClass->AddProperty(pClientManager, _gstr("modelIndex"), ARGUMENT_STRING, FunctionEntityGetModel, FunctionEntitySetModel);
 }
