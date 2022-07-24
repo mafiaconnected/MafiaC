deee54360cc9d341c5e9aeb7e8b000877db8a115
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-24 09:38:25 -0500

    Disable loading screens for now

--- Projects/Hacks/MultiplayerModTwo/LoadScreen.cpp
+++ Projects/Hacks/MultiplayerModTwo/LoadScreen.cpp
@@ -13,28 +13,28 @@ void CLoadScreen::UpdateProgress(float progress)
 
 // To-do: Make the loadscreen actually draw itself
 
-void CLoadScreen:: LoadResources()
+void CLoadScreen::LoadResources()
 {
-	auto pStream = Strong<Stream>::New(g_pContext->GetFileSystem()->Open(_gstr("/GUI/Images/LoadScreen.png"), false));
-	if (pStream)
-	{
-		backgroundTex = Texture::CreateFromPNG(pStream, false);
-		if (backgroundTex)
-		{
-			backgroundTex->SetName(_gstr("LoadScreen.png"));
-		}
-	}
+	//auto pStream = Strong<Stream>::New(g_pContext->GetFileSystem()->Open(_gstr("/GUI/Images/LoadScreen.png"), false));
+	//if (pStream)
+	//{
+	//	backgroundTex = Texture::CreateFromPNG(pStream, false);
+	//	if (backgroundTex)
+	//	{
+	//		backgroundTex->SetName(_gstr("LoadScreen.png"));
+	//	}
+	//}
 }
 
 void CLoadScreen::Draw(Galactic3D::C2D* p2D)
 {
-	if (!p2D && !backgroundTex) return;
+	//if (!p2D && !backgroundTex) return;
 
-	if (loadProgress > 0.0f && loadProgress < 1.0f)
-	{
-		//float scrWidth = (float)MafiaSDK::GetIGraph()->Scrn_sx();
-		//float scrHeight = (float)MafiaSDK::GetIGraph()->Scrn_sy();
+	//if (loadProgress > 0.0f && loadProgress < 1.0f)
+	//{
+	//	float scrWidth = (float)MafiaSDK::GetIGraph()->Scrn_sx();
+	//	float scrHeight = (float)MafiaSDK::GetIGraph()->Scrn_sy();
 
-		//p2D->Draw2DImage(backgroundTex, { 0, 0 }, { scrWidth, scrHeight });
-	}
+	//	p2D->Draw2DImage(backgroundTex, { 0, 0 }, { scrWidth, scrHeight });
+	//}
 }
