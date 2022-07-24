1f5d589c757c4091bba941ed0b961e46791d7244
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-07-24 09:36:50 -0500

    Remove Mafia 1 util from Mafia 2 project

--- Projects/Hacks/MultiplayerModTwo/Utils/MSDK.cpp
+++ Projects/Hacks/MultiplayerModTwo/Utils/MSDK.cpp
@@ -1,13 +1,7 @@
 #include "stdafx.h"
 
 using namespace M2;
-extern SDL_Window* g_pWindow;
 
 namespace Mafia2SDK
 {
-    bool IsWindowFocused()
-    {
-        Uint32 iWindowFlags = SDL_GetWindowFlags(g_pWindow);
-        return (iWindowFlags & SDL_WINDOW_INPUT_FOCUS | SDL_WINDOW_MOUSE_FOCUS);
-    }
 }
