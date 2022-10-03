9cbcd71455b3c0dc03a9f10c42a64ff68ba01fad
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-03 16:15:08 -0500

    Update bat files

--- Configure_VS2019.bat
+++ Configure_VS2019.bat
@@ -2,5 +2,6 @@
 set CMAKE_GENERATOR="Visual Studio 16 2019"
 set CMAKE_GENERATOR_PLATFORM=Win32
 set CMAKE_GENERATOR_TOOLSET=v142
+set CMAKE_EXTRA_ARGS=
 call Configure.bat
-pause
+if not defined AUTOMATION pause
