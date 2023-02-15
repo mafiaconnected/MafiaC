1efa4153648bcff5d5568e59fa2f197c8767ae91
Jack Powell <jack9267@users.noreply.github.com>
2023-02-15 17:16:54 +0000

    Remove symbols

--- Update/Build Update.bat
+++ Update/Build Update.bat
@@ -3,8 +3,8 @@ cd ..\Lib\x86\v141_xp\Release
 del preupdate.tar
 del postupdate.tar
 cls
-"..\..\..\..\Tools\7z.exe" a preupdate.tar Updater.exe Updater.pdb ..\..\..\..\Update\removed.txt
-"..\..\..\..\Tools\7z.exe" a postupdate.tar -xr!DebugTest.dll -xr!d3d8to9.dll -xr!MafiaC.pdb -xr!Launcher.pdb -xr!mozjs-*.pdb -xr!Updater.pdb Launcher.exe *.dll *.pdb Hacks\*.dll Engine\*.exe Engine\*.dll ..\..\..\..\Setup\resources MafiaC.tar cacert.pem
+"..\..\..\..\Tools\7z.exe" a preupdate.tar Updater.exe ..\..\..\..\Update\removed.txt
+"..\..\..\..\Tools\7z.exe" a postupdate.tar -xr!DebugTest.dll -xr!d3d8to9.dll Launcher.exe *.dll Hacks\*.dll Engine\*.exe Engine\*.dll ..\..\..\..\Setup\resources MafiaC.tar cacert.pem
 "..\..\..\..\Tools\7z.exe" a update.tar preupdate.tar postupdate.tar
 "..\..\..\..\Tools\7z.exe" a update.tar.gz update.tar
 del preupdate.tar
