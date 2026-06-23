4b5a91b29957eaec0b86503afd097f780cf0d0e4
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2026-06-23 17:46:35 -0500

    Log msg

--- Projects/Hacks/MultiplayerModOne/Hacks/Hooks.cpp
+++ Projects/Hacks/MultiplayerModOne/Hacks/Hooks.cpp
@@ -165,12 +165,16 @@ RAWCODECALL HumanUseActor()
 {
 	if (g_pClientGame->m_bUseActorInvokedByGame)
 	{
+		
+
 		CClientHuman* pClientHuman = g_pClientGame->m_pClientManager->FindHuman((MafiaSDK::C_Human*)g_pHumanUseActor_Human);
 		if (pClientHuman != nullptr)
 		{
 			CClientVehicle* pClientVehicle = g_pClientGame->m_pClientManager->FindVehicle((MafiaSDK::C_Car*)g_pHumanUseActor_Actor);
 			if (pClientVehicle != nullptr)
 			{
+				_glogverboseprintf(_gstr("[GAME] HumanUseActor - Human %d used Vehicle %d with action %d. Extra2: %d, Extra 3: %d\n"), pClientHuman->GetId(), pClientVehicle->GetId(), g_pHumanUseActor_Unk1, g_pHumanUseActor_Unk2, g_pHumanUseActor_Unk3);
+
 				// To abort, use this:
 				// g_bCancelHumanUseActor = true;
 
